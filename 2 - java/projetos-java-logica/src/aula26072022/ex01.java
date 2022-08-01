9c2492515630a68358cbb3268d0a5aa64f2df191
andrei.welter <andreiwelter9067@gmail.com>
2022-08-01 16:30:58 -0300

    ok

---
+++ 2 - java/projetos-java-logica/src/aula26072022/ex01.java
@@ -0,0 +1,8 @@
+package aula26072022;
+
+public class ex01 {
+    public static void main(String[] args) {
+        System.out.println("O primeiro programa a gente nunca esquece!");
+
+    }
+}
---
+++ 2 - java/projetos-java-logica/src/aula26072022/exemplo.java
@@ -0,0 +1,19 @@
+package aula26072022;
+
+public class exemplo {
+    // ola mundo
+    // comando para gerar o metodo principal
+    // que executa o algoritmo
+    // psvm + TAB
+    public static void main(String[] args) {
+
+        // comando para mostrar algo na tela sem quebra de linha
+        // sout + TAB
+        System.out.print("Ola mundo");
+
+        // com quebra de linha
+        System.out.println("Ola mundo dois");
+
+        System.out.println("Ola mundo tres");
+    }
+}
